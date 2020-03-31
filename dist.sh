80448e67a8c650f08fb3d85c51c3965faf78f0d8
shunf4 <shun1048576@gmail.com>
2020-03-31 16:07:08 +0800

    update

--- dist.sh
+++ dist.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-# pyinstaller --distpath . --specpath ./spec --name _myinit -F myinit.py
-pyinstaller --specpath ./spec myinit.py
+pyinstaller --distpath . --specpath ./spec --name _myinit -F myinit.py
+# pyinstaller --specpath ./spec myinit.py
 
